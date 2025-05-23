386f77d32bc30136a283a32aaeb02347ecbd7204
Mohammad Aman <mohammadamaan1105@gmail.com>
2025-05-23 10:19:44 +0545

    Implement invoice page with search, pagination, and table components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: "incremental"  // To enable this run pnpm install next@canary
+  // }
+
 };
 
 export default nextConfig;
